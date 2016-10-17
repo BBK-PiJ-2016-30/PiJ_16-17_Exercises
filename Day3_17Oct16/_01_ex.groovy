28c84e5a7a354985e66f6f51b9cdb4b8f9479140
dbucha01 <dbucha01@dcs.bbk.ac.uk>
2016-10-17 19:38:56 +0100

    update ex1 day 3

--- Day3_17Oct16/_01_ex.groovy
+++ Day3_17Oct16/_01_ex.groovy
@@ -1,7 +1,14 @@
+//Equality of floating-point numbers
+//showing why floats can't be used for equality
+
 double d1 = 1.255
 double d2 = d1 +7 -4 -3
+
 if(d1==d2){
 	println("1.255 is equal to 1.255 plus 7 minus 7");
 } else {
-	println("1.255 is NOT equal to 1.255 plus 7 minus 7);
+	println("1.255 is NOT equal to 1.255 plus 7 minus 7");
 }
+
+println d1
+println d2
