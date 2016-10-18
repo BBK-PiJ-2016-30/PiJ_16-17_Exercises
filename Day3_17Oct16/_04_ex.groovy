10f4e0818c7ab26df9207418647c5b203e3fc092
BBK-PiJ-2016-30 <dbucha01@mail.bbk.ac.uk>
2016-10-18 15:42:38 +0100

    exercise 4, closest distance

--- Day3_17Oct16/_04_ex.groovy
+++ Day3_17Oct16/_04_ex.groovy
@@ -4,10 +4,7 @@
 
 //some variable declarations
 String str = ""
-def pointList = ["+","-","*", "/"]
-int option = 0
-int operatorLoc = 0
-int strLen = 0
+def pointList = []
 
 //1. declare class
 class Point{
@@ -15,16 +12,70 @@ class Point{
 	double y;
 }
 
-//2. create points
-for(i=0; i<3; i++){
-	print("Please enter Point " + i + " in the format - x,y : ");
-	str = System.console().readLine()
-	def system = new Point()
-	systems.inetAddress = params."point+${i}"	
+def splitPoints(userString,option){
+	int operatorLoc = 0
+	operatorLoc = userString.indexOf(",")
+	
+	if(option == 1){
+		return Double.parseDouble(userString[0..operatorLoc-1])
+	}else{
+		return Double.parseDouble(userString[operatorLoc+1..userString.length()-1])
 
+	}
 }
 
-println point1
-println point2
-println point3
+
+
+//2. create points
+
+print("Please enter Point 1 in the format - x,y : ");
+str = System.console().readLine()
+Point newP1 = new Point()
+newP1.x = splitPoints(str,1)
+newP1.y = splitPoints(str,2)
+
+print("Please enter Point 2 in the format - x,y : ");
+str = System.console().readLine()
+Point newP2 = new Point()
+newP2.x = splitPoints(str,1)
+newP2.y = splitPoints(str,2)
+
+print("Please enter Point 3 in the format - x,y : ");
+str = System.console().readLine()
+Point newP3 = new Point()
+newP3.x = splitPoints(str,1)
+newP3.y = splitPoints(str,2)
+
+
+//3. Calculate distance
+//point 1 - point 2
+double dist1 = Math.sqrt(((newP1.x - newP2.x)**2)+((newP1.y - newP2.y)**2))
+pointList.push(dist1)
+
+//point 1 - point 3
+double dist2 = Math.sqrt(((newP1.x - newP3.x)**2)+((newP1.y - newP3.y)**2))
+pointList.push(dist2)
+
+//point 3 - point 2
+double dist3 = Math.sqrt(((newP3.x - newP2.x)**2)+((newP3.y - newP2.y)**2))
+pointList.push(dist3)
+
+
+//4.print closest pair
+int minDistIndex = pointList.indexOf(pointList.min())
+
+switch(minDistIndex+1){
+	case 1:
+		println("Point 1 and Point 2 are closest.")
+		break;
+	case 2:
+		println("Point 1 and Point 3 are closest.")
+		break;
+	case 3:
+		println("Point 2 and Point 3 are closest.")
+		break;
+	Default:
+		println("Crash, ahhhhh!")
+		break;
+}
 
