5029a68224e236b2dbfc958e41ca73c15f49e35d
dbucha01 <dbucha01@dcs.bbk.ac.uk>
2016-10-31 18:10:30 +0000

    file frame for Calculator class

---
+++ Day5_31Oct16/Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator{
+
+	void add(int a, int b){
+
+	}
+
+	void subtract(int a, int b){
+
+	}
+
+	void multiply(int a, int b){
+
+	}
+
+	void divide(int a, int b){
+
+	}
+
+	void modulus(int a, int b){
+
+	}
+
+}
