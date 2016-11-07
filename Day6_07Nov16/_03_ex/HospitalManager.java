bb827ebea564f4cd9448da6fddfbb47928deebe3
BBK-PiJ-2016-30 <dbucha01@dcs.bbk.ac.uk>
2016-11-07 20:53:45 +0000

    partial answer to exercise 3

---
+++ Day6_07Nov16/_03_ex/HospitalManager.java
@@ -0,0 +1,122 @@
+public class HospitalManager {
+	private Patient firstPatient = null;
+	private int patQueue = 0;
+	
+	// other methods come here...
+	// this is a member method of class HospitalManager
+	public void addPatient(Patient newPatient) {
+		Patient current = this.firstPatient;
+		
+		if (firstPatient == null) {
+			firstPatient = newPatient;
+			this.patQueue ++;
+			return;
+		}
+		
+		while (current.getNextPatient() != null) {
+			// this means we are not yet at the end of the list
+			current = current.getNextPatient();
+		}
+		
+		// at this point, current points to the last patient
+		current.setNextPatient(newPatient);
+		newPatient.setPrevPatient(current);
+		this.patQueue ++;
+		
+	}
+	
+	// this is a member method of class HospitalManager
+	// returns true if the patient was found and removed, false otherwise
+	public boolean deletePatient(String name) {
+		if (firstPatient == null) {
+			// list is empty, nothing to remove
+			return false;
+		}
+		
+		if (firstPatient.getName().equals(name)) {
+			// first patient in the list must be removed
+			firstPatient = firstPatient.getNextPatient();
+			this.patQueue --;
+			return true;
+		}
+		
+		Patient current = firstPatient;
+		while (current.getNextPatient() != null) {
+			if (current.getNextPatient().getName().equals(name)) {
+				// We found it! It is the next one!
+				// Now link this patient to the one after the next
+				current.setNextPatient(current.getNextPatient().getNextPatient());
+				this.patQueue --;
+				return true;
+			}
+			current = current.getNextPatient();
+		}
+		return false;
+	}
+	
+	public Patient getFirstPatient(){
+		return(firstPatient);
+	}
+	
+	public int getQueue(){
+		return(this.patQueue);
+	}
+	
+	public void printPatientList (){
+			
+			if(firstPatient==null){
+				System.out.println("No one in the hosipital.");
+			}
+			
+			Patient current = firstPatient;
+			do{
+				System.out.println("Patient: ");
+				System.out.print(current.getName() + " - ");
+				System.out.print(current.getAge() + " - ");
+				System.out.println(current.getIllness());
+				current = current.getNextPatient;
+			}while(current != null)
+	
+	}
+	
+	public static void main(String [] args){
+		
+		HospitalManager newHosp = new HospitalManager();
+		//arrPat = new Patient[10];
+		//Patient pat1 = new Patient("Pat1","20","Unknown")
+		
+		for(int i=0; i<10; i++){
+			String str = ""
+			switch(i%3){
+				case 0:
+					str = "Broken Arm";
+					break;
+				case 1:
+					str = "Broken Leg";
+					break;
+				case 2:
+					str = "Broken Nose";
+					break;
+				default:
+					System.out.println("This should never run")
+					break;
+			}
+			Patient newPat = new Patient(("Pat_"+i), (20+i), str);
+			newHosp.addPatient(newPat);
+		}
+		
+
+		newHosp.printPatientList();
+		System.out.println("Patients in queue: " + newHosp.getQueue());
+		
+		newHosp.deletePatient("Pat5");
+		newHosp.deletePatient("Pat7");
+		
+		System.out.println("\n Patients in hospital after deletion");
+		newHosp.printPatientList();
+		
+		
+		System.out.println("Patients in queue: " + newHosp.getQueue());
+	}
+
+}
