49606e4f1bf1182be1740aaa45e22565f9c52808
BBK-PiJ-2016-30 <dbucha01@mail.bbk.ac.uk>
2016-11-27 12:48:32 +0000

    ex7 tidied code even though it doesn't compile

--- Day8_21Nov16/_07_ex/StringExtend.java
+++ Day8_21Nov16/_07_ex/StringExtend.java
@@ -5,10 +5,8 @@ public class StringExtend extends String {
 	*prints on the screen the even numbered characters of string
 	*/
 	public void printEven(String str){
-		for(int i = 0; i < str.length(); i++){
-			if(i/2 == 0){
-				System.out.println(i);
-			}
+		for(int i = 0; i < str.length(); i+= 2){
+				System.out.println(str.charAt(i));
 		}
 		
 	}
